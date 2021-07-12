ca3052457a6ed40b57ba79e6bc2ba14d74e61bb0
Kate <katelynrpeterson16@gmail.com>
2021-07-11 22:02:52 -0400

    Add button for light/dark mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { IconButton } from "@chakra-ui/react";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { FaSun, FaMoon, FaInstagram, FaGithub, FaLinkedIn } from 'react-icons/fa';
+import { Flex, VStack, Heading } from '@chakra-ui/layout';
 import React from 'react';
 
 const App = () => {
-    return <div>Inside App</div>
+    const { colorMode, toggleColorMode } = useColorMode();
+    const isDark = colorMode === 'dark';
+
+    return (
+        <VStack p={5}>
+            <Flex w="100%">
+                <Heading 
+                    ml="8" size="md" fontWeight="semibold" color="cyan.400">
+                        KP Page
+                </Heading>
+            </Flex>
+        <IconButton icon={isDark ? <FaSun /> : <FaMoon />} isRound={true} onClick={toggleColorMode}></IconButton>
+        </VStack>
+
+    )
 };
 
 export default App;
