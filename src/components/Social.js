9ba0dc7200321e24d27b863019860053e65b6931
Kate <katelynrpeterson16@gmail.com>
2021-07-14 12:11:21 -0400

    Add social media icons

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,10 +1,15 @@
+import { HStack } from '@chakra-ui/layout';
+import { Icon } from '@chakra-ui/react'
 import React from 'react';
+import {FaFacebookF, FaGoogle, FaSpotify} from 'react-icons/fa'
 
 function Social() {
     return (
-        <div>
-            Inside Social Component
-        </div>
+        <HStack spacing="24">
+            <Icon as={FaFacebookF} boxSize="50"></Icon>
+            <Icon as={FaGoogle} boxSize="50"></Icon>
+            <Icon as={FaSpotify} boxSize="50"></Icon>
+        </HStack>
     )
 }
 
