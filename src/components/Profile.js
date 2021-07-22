464e045ea7743dfb15c01ee49472c67e4031e6df
Kate <katelynrpeterson16@gmail.com>
2021-07-22 10:39:45 -0600

    Add profile descriptom

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Flex, Box, Heading, Text } from '@chakra-ui/layout'
+import { useMediaQuery } from '@chakra-ui/media-query';
 
 function Profile() {
+
+    const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)")
+
     return (
-        <div>
-            Inside Profile Component
-        </div>
+        <Flex 
+            direction={isNotSmallerScreen ? "row" : "column"}
+            w="100%"
+            maxWidth={{base: "100vh", md:"130vh", lg:"130vh", xl:"130vh"}}
+        >
+          <Box alignSelf="center" px="32" py="16">
+              <Heading>7+</Heading>
+              <Text fontSize="2xl" color="gray.400"> Years Experience</Text>
+          </Box>
+          <Box alignSelf="center" px="32" py="16">
+              <Text fontWeight="bold" fontSize="2xl">Developer specializing in Ruby and JavScript</Text>
+
+          </Box>
+
+        </Flex>
     )
 }
 
